804660550cc72a9b5fa2af7d639fed33e17a194a
Rahul Chaurasiya <117914520+irahulchaurasiya@users.noreply.github.com>
2024-02-19 16:44:46 +0530

    Delete wishlist directory

--- wishlist/src/main/java/com/xindus/wishlist/Repositories/WishlistRepository.java
+++
@@ -1,9 +0,0 @@
-package com.xindus.wishlist.Repositories;
-
-import com.xindus.wishlist.Models.Wishlist;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WishlistRepository extends JpaRepository<Wishlist,Long> {
-}
